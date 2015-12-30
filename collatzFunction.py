752582f2fc57a285a13c4e9da3ff09225770fe82
Landon Powell <landonpowell@linuxmail.org>
2015-12-30 11:30:08 -0500

    Update collatzFunction.py

--- collatzFunction.py
+++ collatzFunction.py
@@ -1,7 +1,4 @@
 def c(x):
     if x%2: c(x*3+1) # Odd numbers.
     elif int(x)&int(x)-1: c(x/2) # Even but not a power of two.
-    else: return "confirm" 
-# Five lines!
-# Example 
-c(9043)
+    else: print("confirm")
