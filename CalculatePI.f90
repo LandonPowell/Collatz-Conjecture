b030eed408592afb961a599c8bc0e654c0a1614d
Landon Powell <landonjpowell@airmail.cc>
2016-03-14 14:35:53 -0400

    Calculates the value of Pi to 32 decimal places.
    Written in Fortran. Self Documenting.

---
+++ CalculatePI.f90
@@ -0,0 +1,13 @@
+program calculatePi
+    integer   :: counter = 0
+    real (16) :: pi = 0.0
+    do while (.TRUE.)
+        if ( modulo(counter, 2) == 0 ) then
+            pi = pi + 4.0/(counter*2+1)
+        else
+            pi = pi - 4.0/(counter*2+1)
+        end if
+        counter = counter + 1
+        print *, pi
+    end do
+end program calculatePi
