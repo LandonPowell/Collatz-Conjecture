65a3bb2297cb6685332cdeab3afcf3af559ee55b
Landon Powell <landonjpowell@airmail.cc>
2016-03-26 04:07:00 -0400

    Update CalculatePi.f90

--- CalculatePi.f90
+++ CalculatePi.f90
@@ -1,13 +1,20 @@
 program calculatePi
+    
+    ! This is an implementation fo the Leibniz Formula for Pi
+    
     integer   :: counter = 0
     real (16) :: pi = 0.0
+
     do while (.TRUE.)
         if ( modulo(counter, 2) == 0 ) then
             pi = pi + 4.0/(counter*2+1)
         else
             pi = pi - 4.0/(counter*2+1)
         end if
+
         counter = counter + 1
         print *, pi
+
     end do
+
 end program calculatePi
